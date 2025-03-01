51c443fffa3286e66dbc77d38b3d1474877c6f7f
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-03-01 01:00:43 -0800

    new list ocmponet

---
+++ components/sidebar/list.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useOrganizationList } from "@clerk/nextjs";
+import Item from "./item";
+
+export const List = () => {
+  const { userMemberships } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    }
+  });
+
+  if(!userMemberships.data?.length) {
+    return null;
+  }
+
+  return (
+    <ul className="space-y-4">
+      {userMemberships.data.map((m => (
+        <Item key={m.organization.id} name={m.organization.name} id={m.organization.id} imageUrl={m.organization.imageUrl} />
+      )))}
+    </ul>
+  );
+}
